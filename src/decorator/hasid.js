2610aeb73f2ee882ba29269c2f907fa99d96f4b2
yusangeng <yusangeng@outlook.com>
2018-01-15 22:22:35 +0800

    bug fix

--- src/decorator/hasid.js
+++ src/decorator/hasid.js
@@ -2,5 +2,10 @@ import mix from '../mix'
 import HasId from '../mixin/HasId'
 
 export default function hasid (target) {
-  return class DecoratedHasIdClass extends mix(target).with(HasId) {}
+  return class DecoratedHasIdClass extends mix(target).with(HasId) {
+    constructor (...params) {
+      super(...params)
+      this.initId() // 此处不支持设置id
+    }
+  }
 }
